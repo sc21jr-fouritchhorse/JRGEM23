2693206e68d4640bac4f45a24380b9ee947d4822
Joe Riley <joerileycode@gmail.com>
2023-01-31 08:13:07 +0000

    add more todo mons

--- draft022023/DML/monsters.sql
+++ draft022023/DML/monsters.sql
@@ -2,24 +2,60 @@
 
 Rat
     BEAST type
+(Evolves into \/)
+Were-rat
+    BEAST type
+(or)
+Cybrat
+    BEAST/INDUST type
+(Evolves into \/)
+Verminator
+    BEAST/INDUST type
+
+(Synth 4 rats)
+Rat-King
+    BEAST/UNDEAD type
+
 Pidgeon
     BEAST/WIND type
+(Evolves into \/)
+Doh-doh
+    BEAST/NATURE type
+(Evolves into \/)
+Mountain-devil
+    NATURE/WIND type
+
 Cigarette
     FLAME type
+(Evolves into \/)
 Pack-O'-Cigarettes
     FLAME type
+(Evolves into \/)
 Nichomet
     FLAME/DEMON type
+
 Uprooted-weed
     PLANT type
+(Evolves into \/)
 Boisterous-weed
     PLANT/BEAST type
+(Evolves into \/)
 Dande-lion
     PLANT/BEAST type
+
 Cracked-bottle
     WATER type
+(Evolves into \/)
 Spillage
     WATER/SPIRIT type
+(Evolves into \/)
 Mr-Bubbly
     WATER/SPIRIT type
+
+Electric sheep
+    ELEC/BEAST
+(synth w/ the child of 2 electric sheep \/)
+Three electric sheep stuck together
+    ELEC/BEAST
+
 */
