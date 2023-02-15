c14c2d4e698742902b28320d20b14336a86b93ac
Joe Riley <sc21jr@leeds.ac.uk>
2023-02-15 19:45:15 +0000

    fix the python script so that all data inserted so far is present

--- python/db_init.py
+++ python/db_init.py
@@ -4,7 +4,6 @@ import glob
 import re
 import os
 import sqlite3
-import sqlparse
 
 sql_dir = '../SQL/'
 table_dir = 'DDL/'
@@ -13,8 +12,8 @@ data_dir = 'DML/'
 
 def load_queries_from_file(filename):
     with open(filename, 'r') as f:
-        queries = sqlparse.split(f.read())
-    return [q for q in queries if q.strip()]
+        queries = f.read().split(';')
+    return queries
 
 
 def get_priority(filename):
@@ -28,16 +27,21 @@ def get_priority(filename):
 
 
 table_files = sorted(glob.glob(sql_dir + table_dir + '*.sql'), key=get_priority)
-data_files = sorted(glob.glob(sql_dir + data_dir + '*.sql'))
+data_files = glob.glob(sql_dir + data_dir + '*.sql')
 
 all_queries = []
 
 for table_file in table_files:
-    all_queries.extend(load_queries_from_file(table_file))
-
-for data_file in data_files:
-    all_queries.extend(load_queries_from_file(data_file))
-
+    print(table_file + '\n')
+    table_queries = load_queries_from_file(table_file);
+    all_queries.extend(table_queries)
+    for data_file in data_files:
+        file_title = data_file.replace('_populate.sql', '').replace(data_dir, '')
+        print('\t' + file_title)
+        if table_file.replace('_init.sql', '').replace(table_dir, '') == file_title:
+            data_queries = load_queries_from_file(data_file)
+            all_queries.extend(data_queries)
+            data_files.remove(data_file)
 
 database_name = '../draft23.db'
 
